de7f45b62440d850915c53c4d781caa5f1b1fa5a
Liviu Cristian Gavrilet <liviu.gavrilet@gmail.com>
2022-11-03 00:32:28 +0200

    added the code!

---
+++ src/linearregressioncalculator/LinearRegressionCalculator.java
@@ -0,0 +1,14 @@
+package linearregressioncalculator;
+
+public class LinearRegressionCalculator {
+
+    public static RegressionCalculus regCalc = new RegressionCalculus();
+    public static void main(String[] args) {
+        
+        
+        new GraphicalInterface().setVisible(true);
+        
+        
+    }
+    
+}
